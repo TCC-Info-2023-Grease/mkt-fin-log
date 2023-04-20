ab2a08987ede68b3c51582c744579e13e053b562
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-04-20 16:33:36 -0300

    Update head.php

--- grease/resources/views/components/head.php
+++ grease/resources/views/components/head.php
@@ -1,3 +1,8 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
+
+<link rel="stylesheet" href="sweetalert2.min.css">
+<script src="sweetalert2.min.js"></script>
