d25158664a7c5964a3d8df9457fa34ae05f156e2
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-04-20 17:17:06 -0300

    Update CadastroControlador.php

--- grease/controlador/CadastroControlador.php
+++ grease/controlador/CadastroControlador.php
@@ -1,27 +1,42 @@
 <?php
+# ------ Dados Iniciais
 require '../config.php';
 require '../database/db.php';
 
 global $mysqli;
+import_utils([ 'valida_campo', 'navegate' ]);
 
-import_utils([ 'valida_campo' ]);
 
-$campos_validos =  
+# ------ Validar Envio de Dados
+$campos_validos =  (
     valida_campo($_POST['username'])  && 
     valida_campo($_POST['password']) && 
     valida_campo($_POST['phone'])    &&
     valida_campo($_POST['age'])      &&
     valida_campo($_POST['genrer'])
-;
-
+);
 if (!$campos_validos) 
 {
-    header('Location: ' . $_ENV['URL_VIEWS'] . '/cadastrar.php?erro=campos_invalidos');
+    navegate($_ENV['URL_VIEWS'] . '/cadastrar.php?erro=campos_invalidos');
 } 
+
+
+# ----- Cadastro 
 
 $usuario = new Usuario($mysqli);
 
 if (!$usuario->unico('email', $_POST['email'])) 
 {
-    header('Location: '. $_ENV['URL_VIEWS']. '/cadastrar.php?erro=erro_usuario');
+    navegate($_ENV['URL_VIEWS']. '/cadastrar.php?erro=erro_usuario');
 }
+
+$usuario->cadastrar_visitante([
+    'username' => $_POST['username'],
+    'email'    => $_POST['email'],
+    'password' => $_POST['password'],
+    'phone'    => $_POST['phone'],
+    'age'      => $_POST['age'],
+    'genrer'   => $_POST['genrer']
+]);
+
+navegate($_ENV['URL_VIEWS']. '/home.php');
