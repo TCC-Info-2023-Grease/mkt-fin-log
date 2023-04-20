35e2f7d92b239a416119dd6d9816f00392ccf579
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-04-20 16:32:44 -0300

    Update CadastroControlador.php

--- grease/controlador/CadastroControlador.php
+++ grease/controlador/CadastroControlador.php
@@ -1,10 +1,27 @@
 <?php
 require '../config.php';
+require '../database/db.php';
+
+global $mysqli;
 
 import_utils([ 'valida_campo' ]);
 
-if (!valida_campo($_GET['nome']) && !valida_campo($_GET['email']) && !valida_campo($_GET['email'])) 
+$campos_validos =  
+    valida_campo($_POST['username'])  && 
+    valida_campo($_POST['password']) && 
+    valida_campo($_POST['phone'])    &&
+    valida_campo($_POST['age'])      &&
+    valida_campo($_POST['genrer'])
+;
+
+if (!$campos_validos) 
 {
+    header('Location: ' . $_ENV['URL_VIEWS'] . '/cadastrar.php?erro=campos_invalidos');
+} 
 
+$usuario = new Usuario($mysqli);
+
+if (!$usuario->unico('email', $_POST['email'])) 
+{
+    header('Location: '. $_ENV['URL_VIEWS']. '/cadastrar.php?erro=erro_usuario');
 }
-
