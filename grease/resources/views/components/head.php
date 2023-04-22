61eea0cb997ba5b0e1b36f54568ca772d027a70a
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-04-21 23:23:01 -0300

    views: add meta tags for SEO

--- grease/resources/views/components/head.php
+++ grease/resources/views/components/head.php
@@ -1,8 +1,65 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
- 
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
+<!-- # Meta Tags # -->
+<meta 
+  charset="UTF-8" />
+<meta 
+  name="description" 
+  content="Site do TCC 3° Info B - 2023. Musical Grease sistema sobre as finanças e sobre o musical em geral" />
+<meta 
+  name="keywords" 
+  content="grease, tcc, finanças, marketing, entretenimento" />
+<meta 
+  name="author" 
+  content="Nome dos autores do TCC" />
+<meta 
+  name="viewport" 
+  content="width=device-width, initial-scale=1.0"/>
+<meta 
+  name="robots" 
+  content="index, follow" />
+<meta 
+  name="googlebot" 
+  content="index, follow" />
+<meta 
+  name="bingbot" 
+  content="index, follow" />
+<meta 
+  name="apple-mobile-web-app-title" 
+  content="Grease" />
+<meta 
+  name="application-name" 
+  content="Grease" />
+<meta 
+  name="theme-color" 
+  content="#000000" />
+<!-- # /Meta Tags # -->
 
-<link rel="stylesheet" href="sweetalert2.min.css">
-<script src="sweetalert2.min.js"></script>
+
+<!-- # Link # -->
+<link 
+  rel="canonical" 
+  href="https://exemplo.com/" />
+<link 
+  rel="alternate" 
+  type="application/rss+xml" 
+  href="https://exemplo.com/rss.xml" 
+  title="RSS Feed do Grease" />
+<link 
+  rel="icon" 
+  type="image/png" 
+  sizes="32x32" 
+  href="/favicon-32x32.png" />
+<!-- # /Link # -->
+
+
+<!-- # JQUERY # -->
+<script 
+  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+</script>
+<!-- # /JQUERY # -->
+
+
+<!-- # Sweet Alert # -->
+<script 
+  src="https://cdn.jsdelivr.net/npm/sweetalert2@11">
+</script> 
+<!-- # /Sweet Alert # -->
