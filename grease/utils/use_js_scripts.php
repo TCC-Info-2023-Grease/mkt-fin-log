1267089e92654d9865aa56f753fd9d19dda5c388
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-04-22 03:45:30 -0300

    fix: error string

--- grease/utils/use_js_scripts.php
+++ grease/utils/use_js_scripts.php
@@ -5,11 +5,11 @@ function use_js_scripts($scripts = [], $list_scripts = []) {
 
   for ($s = 0; $s < count($scripts); $s++) {
     echo '
-      <!-- '. $scripts .'-->
+      <!-- '. $scripts[$s] .'-->
       <script
         src="'. $list_scripts[$scripts[$s]]  .'">
       </script>
-      <!-- /'.  $scripts .' -->
+      <!-- /'.  $scripts[$s] .' -->
     ';
   }
 }
