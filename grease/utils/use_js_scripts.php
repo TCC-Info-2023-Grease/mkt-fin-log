d60c3b1ec9fe5eb20acf7b5d608a61e5d46bfed0
Gustavo H. J <70487766+MrNullus@users.noreply.github.com>
2023-05-09 19:12:43 -0300

    changes

--- grease/utils/use_js_scripts.php
+++ grease/utils/use_js_scripts.php
@@ -1,12 +1,19 @@
 <?php
 
-function use_js_scripts($scripts = [], $list_scripts = []) {
-
-
+/**
+ * Responsável por adicionar os arquivos JavaScript
+ *
+ * @param array $scripts
+ * @param array $list_scripts
+ * @param string $type
+ * @return void
+ */          
+function use_js_scripts($scripts = [], $list_scripts = [], $type = 'text/javascript') {
   for ($s = 0; $s < count($scripts); $s++) {
     echo '
       <!-- '. $scripts[$s] .'-->
       <script
+        type="'. $type .'"
         src="'. $list_scripts[$scripts[$s]]  .'">
       </script>
       <!-- /'.  $scripts[$s] .' -->
