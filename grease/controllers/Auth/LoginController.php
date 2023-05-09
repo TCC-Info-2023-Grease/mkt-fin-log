e12bfd25b3e9f7e86afe69696aa67104c0a21f40
Gustavo H. J <70487766+MrNullus@users.noreply.github.com>
2023-05-09 17:36:02 -0300

    Update LoginController.php

--- grease/controllers/Auth/LoginController.php
+++ grease/controllers/Auth/LoginController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 # ------ Dados Iniciais
 require '../../config.php';
@@ -31,37 +30,3 @@ if (!$usuario->login($dados['email'], $dados['password']))
 }
 
 navegate($_ENV['URL_VIEWS']. '/visitante/home.php');
-=======
-<?php
-# ------ Dados Iniciais
-require '../../config.php';
-
-global $mysqli;
-import_utils([ 'valida_campo', 'navegate' ]);
-
-
-# ------ Validar Envio de Dados
-$campos_validos =  (
-    valida_campo($_POST['email'])  && 
-    valida_campo($_POST['password']) 
-);
-if (!$campos_validos) 
-{
-    navegate($_ENV['URL_VIEWS'] . '/auth/login.php?erro=campos_invalidos');
-} 
-
-
-# ----- Login 
-$usuario = new Usuario($mysqli);
-$dados = [ 
-    'email' => $_POST['email'] , 
-    'password' => $_POST['password'] 
-];
-
-if (!$usuario->login($dados['email'], $dados['password']))
-{
-    navegate($_ENV['URL_VIEWS']. '/auth/login.php?erro=usuario');
-}
-
-navegate($_ENV['URL_VIEWS']. '/visitante/home.php');
->>>>>>> 286a4901e05e7d84006a15f932d5b2227f5e0c7a
