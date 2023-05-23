94b072aa26059294c911ea50f69740ae4467f58f
Gustavo H. J <70487766+MrNullus@users.noreply.github.com>
2023-05-23 14:38:55 -0300

    Add files via upload

--- grease/resources/views/visitante/home.php
+++ grease/resources/views/visitante/home.php
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-<?php
-# ------ Configurações Básicas
-require dirname(dirname(dirname(__DIR__))) . '/config.php';
-global $_ENV;
-
-import_utils(['extend_styles', 'render_component']);
-?>
-
-<!------- HEAD --------->
-<?php
-render_component('head');
-extend_styles(['styles']);
-?>
-
-<title>
-    
-</title>
-<script>
-    $(document).ready(function () {
-        $('.phone').inputmask('(99) - 99999-9999');
-    });  
-</script>
-<!-------/ HEAD --------->
-
-
-<!------- BODY --------->
-
-<body>
-    <div>
-        Olá
-    </div>
-</body>
-<!------- /BODY --------->
-=======
 <?php
 # ------ Configurações Básicas
 require dirname(dirname(dirname(__DIR__))) . '/config.php';
@@ -61,4 +26,3 @@ extend_styles(['styles']);
     </div>
 </body>
 <!------- /BODY --------->
->>>>>>> 286a4901e05e7d84006a15f932d5b2227f5e0c7a
