9ed1e7b0c23dbd080667151573fd869e1c82a9bf
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-17 21:17:14 -0300

    changes

---
+++ grease/resources/views/components/sobrenos.php
@@ -0,0 +1,54 @@
+<!--COMEÇO SOBRE NOS-->
+<br><br><br>
+
+  
+  <div class = "about-wrapper">
+    <div class = "about-left">
+  
+      <div class = "about-left-content">
+        <div>
+          <div class = "shadow2">
+            <div class = "about-img">
+              <img src = "<?= assets('images/','grease.png') ?>" alt = "about image">
+            </div>
+          </div>
+
+          <div id="greFont">
+              <h2>⇀ O Músical ↼</h2>
+              <h3>3° info tarde</h3>
+          </div>
+        </div>
+
+        <ul class = "icons">
+          <a href="https://www.instagram.com/3_info_t/" target="_blank"> <li><i class = "fab fa-instagram"></i></li></a>
+
+<!--COMEÇO AUDIO-->
+          
+<audio controls>
+  <source src="<?= assets('audio/','baixados.mp3') ?>">
+</audio>
+
+<!--FIM AUDIO-->
+          
+        </ul>
+      </div>
+    </div>
+
+    <div class = "about-right">
+      <h1>olá<span>!</span></h1>
+      <h2>Conheça um pouco sobre a gente</h2>
+      <div class = "about-btns">
+
+      <div class = "about-para">
+
+        <p>Somos o 3º MTEC Informática. Para a tarde, temos o prazer de recebê-lo em nosso site. Nesta seção, você encontrará informações sobre Marketing, Projetos, Bastidores e como administramos nosso dinheiro corretamente.</p>
+        <p>Ao presenciar nosso musical, você nos motiva cada vez mais a realizar nosso projeto da melhor forma possível. Caso deseje ajudar de alguma forma, pode contribuir financeiramente para Finanças.</p>
+      
+      </div>
+    </div>
+  </div>
+
+</div></a>
+
+<!--FIM SOBRE NOS-->
+
