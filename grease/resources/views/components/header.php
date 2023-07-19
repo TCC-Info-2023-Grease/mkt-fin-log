4ff4d128eb4792fa85f8b5c6e8dc5687f06f0d8c
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-18 22:48:59 -0300

    changes

--- grease/resources/views/components/header.php
+++ grease/resources/views/components/header.php
@@ -7,19 +7,19 @@
     <ul class="nav-bar">
       <div class="bg"></div>      
       <?php if (!isset($_SESSION['usuario']) && empty($_SESSION['usuario'])) { ?>
+        <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>welcome">Home</a></li>
         <li><a class="nav-link active" href="<?= $_ENV['ROUTE'] ?>auth.login">Login</a></li>
       <?php } else { ?>
         <?php if ($_SESSION['usuario']['tipo_usuario'] == 'adm') { ?>
-          <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>admin.home">Home</a></li>
+          <li><a class="nav-link active" href="<?= $_ENV['ROUTE'] ?>admin.home">Home</a></li>
           <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>admin.categoria_material.index">Categoria Material</a></li>
           <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>admin.material.index">Material</a></li>
           <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>admin.caixa.index">Caixa</a></li>
           <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>admin.makeof.index">Make Of</a></li>
         <?php } elseif ($_SESSION['usuario']['tipo_usuario'] == 'vis') { ?>
+          <li><a class="nav-link active" href="<?= $_ENV['ROUTE'] ?>admin.home">Home</a></li>
           <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>visitante.financas">Finanças</a></li>
-          <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>home.projeto">Projeto</a></li>
-          <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>visitante.makeof">Make Of</a></li>
-          <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>home.contato">Contato</a></li>
+          <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>visitante.makeof">Projeto</a></li>
         <?php } ?>
         <li><a class="nav-link" href="<?= $_ENV['VIEWS'] ?>/auth/profile.php">Perfil</a></li>
         <li><a class="nav-link" href="<?= $_ENV['ROUTE'] ?>auth.sair">Sair</a></li>
