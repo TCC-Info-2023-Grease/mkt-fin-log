e008fa319ebe3a5d447ed3f8fa2b498f569ac947
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-18 22:49:25 -0300

    changes

--- grease/resources/views/visitante/financas.php
+++ grease/resources/views/visitante/financas.php
@@ -15,63 +15,48 @@ import_utils([
 ]);
 
 include $_ENV['PASTA_CONTROLLER'] . '/Caixa/VisitanteDashboardController.php';
-print_r($data);
+//print_r($data);
 ?>
 
 <!------- HEAD --------->
 <?php
 render_component('head');
-extend_styles([ 'css.admin.financas' ]);
+extend_styles([ 'css.visitante.financas' ]);
 ?>
 <title>
   Finanças 🕺 Grease
 </title>
 <!------- /HEAD --------->
-
+<style type="text/css">
+  header {
+    background: black;
+  }
+</style>
 
 <!------- BODY --------->
 <body>
+  <div class="container">
     <?php
     render_component('header');
     ?>
 
-    <section class="dashboard">
-      <div class="top"> <i class="uil uil-bars sidebar-toggle"></i> </div>
       <div class="dash-content">
-        <div class="overview">
+        <div class="overview" style="padding: 2rem;">
           <div class="title"> <span class="text">Olá, <?= ucfirst($_SESSION['usuario']['nome']); ?></span> </div>
           <div class="boxes">
-            <div 
-              class="box 
-                <?php if ($data['saldo_atual'] < 0): ?>
-                  box2
-                <?php elseif ($data['saldo_atual'] > 0): ?>
-                  box1
-                <?php else: ?>
-                  box3
-                <?php endif; ?>
-            ">
+            <div class="box <?php if ($data['saldo_atual'] < 0): ?>box2<?php elseif ($data['saldo_atual'] > 0): ?>box1<?php else: ?>box3<?php endif; ?>">
               <span class="text">Saldo Atual</span> 
               <span class="number">
                  <?= Money::format($data['saldo_atual']); ?>
               </span> 
             </div>
-            <div 
-              class="box 
-                <?php if ($data['total_gasto'] < $data['total_necessario']): ?>
-                  box1
-                <?php elseif ($data['total_gasto'] > $data['total_necessario']): ?>
-                  box2
-                <?php else: ?>
-                  box3
-                <?php endif; ?>
-            ">              
+            <div class="box <?php if ($data['total_gasto'] < $data['total_necessario']): ?>box1<?php elseif ($data['total_gasto'] > $data['total_necessario']): ?>box2<?php else: ?>box3<?php endif; ?>">              
               <span class="text">Total Gasto</span> 
               <span class="number">
                 <?= Money::format($data['total_gasto']); ?>
               </span> 
             </div>
-            <div class="box box4"> 
+            <div class="box box1"> 
               <span class="text">Total Necessario</span> 
               <span class="number">
                 <?= Money::format($data['total_necessario']); ?>
@@ -79,57 +64,58 @@ extend_styles([ 'css.admin.financas' ]);
             </div>
           </div>
         </div>
-        <div class="activity">
+    
+    <div class="activity">
       <div class="title">
-        <span class="text">Atividades Recentes</span>
-      </div>
-        <div class="activity-data">
-          <div class="data names">
-            <span class="data-title">Usuario</span>
-            
-            <?php foreach ($data['caixa'] as $item): ?>
-            <span class="data-list">
-               <?= $item['nome_usuario']; ?>
-            </span>
-            <?php endforeach; ?>
+            <span class="text">Atividades Recentes</span>
+          </div>
+            <div class="activity-data">
+              <div class="data names">
+                <span class="data-title">Usuario</span>
+                
+                <?php foreach ($data['caixa'] as $item): ?>
+                <span class="data-list">
+                   <?= $item['nome_usuario']; ?>
+                </span>
+                <?php endforeach; ?>
+              </div>
+
+              <div class="data names">
+                <span class="data-title">Valor</span>
+                
+                <?php foreach ($data['caixa'] as $item): ?>
+                <span class="data-list">
+                   <?= Money::format($item['valor']); ?>            
+                 </span>
+                <?php endforeach; ?>
+              </div>
+
+              <div class="data names">
+                <span class="data-title">Data</span>
+                
+                <?php foreach ($data['caixa'] as $item): ?>
+                <span class="data-list">
+                   <?= date('d/m/Y', strtotime($item['data_movimentacao'])); ?>
+                </span>
+                <?php endforeach; ?>
+              </div>
+
+              <div class="data names">
+                <span class="data-title">Tipo Movimentação</span>
+                
+                <?php foreach ($data['caixa'] as $item): ?>
+                <span class="data-list">
+                   <?= $item['tipo_movimentacao']; ?>
+                </span>
+                <?php endforeach; ?>
+              </div>
+          </div>
           </div>
 
-          <div class="data names">
-            <span class="data-title">Valor</span>
-            
-            <?php foreach ($data['caixa'] as $item): ?>
-            <span class="data-list">
-               <?= Money::format($item['valor']); ?>            
-             </span>
-            <?php endforeach; ?>
-          </div>
-
-          <div class="data names">
-            <span class="data-title">Data</span>
-            
-            <?php foreach ($data['caixa'] as $item): ?>
-            <span class="data-list">
-               <?= date('d/m/Y', strtotime($item['data_movimentacao'])); ?>
-            </span>
-            <?php endforeach; ?>
-          </div>
-
-          <div class="data names">
-            <span class="data-title">Tipo Movimentação</span>
-            
-            <?php foreach ($data['caixa'] as $item): ?>
-            <span class="data-list">
-               <?= $item['tipo_movimentacao']; ?>
-            </span>
-            <?php endforeach; ?>
-          </div>
-      </div>
     </div>
   </div>
-</section>
-
   <?php
-  use_js_scripts([ 'js.admin.financas' ]);
+  use_js_scripts([ 'js.visitante.financas' ]);
   ?>
 </body>
 <!------- /BODY --------->
