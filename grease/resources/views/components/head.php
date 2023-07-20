b64e00906dcc0232547e0e2c2dcac528c2439e48
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-19 23:36:58 -0300

    changes

--- grease/resources/views/components/head.php
+++ grease/resources/views/components/head.php
@@ -58,13 +58,15 @@
   rel="stylesheet" 
   href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!-- # /Link # -->
 
 
 <!-- # JQUERY # -->
 <script 
   src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+</script>
+<script 
+  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/jquery.inputmask.bundle.min.js">
 </script>
 <!-- # /JQUERY # -->
 
