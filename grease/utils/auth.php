4a788a8e2ae7ae6b20f3dd20b55808854dc1dfff
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-23 19:23:38 -0300

    changes

--- grease/utils/auth.php
+++ grease/utils/auth.php
@@ -1,23 +1,94 @@
 <?php
-
 class Auth {
 
-  public static function check($user = 'vis') {
+  /**
+   * Verifica se o usuário está autenticado e possui a permissão adequada.
+   *
+   * @param string $user O tipo de usuário permitido ('vis', 'all' ou outro tipo definido).
+   *                     - 'vis': Permite acesso apenas a usuários autenticados.
+   *                     - 'all': Permite acesso a todos os usuários, autenticados ou não.
+   *                     - Outro tipo de usuário: Permite acesso somente a usuários autenticados com o mesmo tipo.
+   * @return bool Retorna true se o usuário estiver autenticado e tiver a permissão adequada, caso contrário, redireciona para a página de login e retorna false.
+   */
+  public static function check($user = 'vis')
+  {
     if (!isset($_SESSION['usuario'])) {
-      header('Location: '. $_ENV['VIEWS'] .'/auth/login.php');
+      self::redirectToLogin();
+      return false;
     }
 
     if ($user === 'all') {
-      if (!isset($_SESSION['usuario'])) {
-        header('Location: '. $_ENV['VIEWS'] .'/auth/login.php');
-      }
+      return true;
+    } elseif ($_SESSION['usuario']['tipo_usuario'] !== $user) {
+      self::redirectToLogin();
+      return false;
     }
 
-    if (!$_SESSION['usuario']['tipo_usuario'] === $user) {
-      header('Location: '. $_ENV['VIEWS'] .'/auth/login.php');
-    }
+    return true;
   }
 
-}
+  /**
+   * Realiza o login do usuário.
+   *
+   * @param array $userData Os dados do usuário a serem armazenados na sessão após o login.
+   * @return void
+   */
+  public static function login($userData)
+  {
+    $_SESSION['usuario'] = $userData;
+  }
 
-?>
+  /**
+   * Faz o logout do usuário.
+   *
+   * @return void
+   */
+  public static function logout()
+  {
+    unset($_SESSION['usuario']);
+    session_destroy();
+    self::redirectToLogin();
+  }
+
+  /**
+   * Verifica se o usuário está logado.
+   *
+   * @return bool Retorna true se o usuário estiver logado, caso contrário, retorna false.
+   */
+  public static function isLogged()
+  {
+    return isset($_SESSION['usuario']);
+  }
+
+  /**
+   * Obtém os dados do usuário autenticado.
+   *
+   * @return array|bool Retorna os dados do usuário autenticado como um array associativo. Se o usuário não estiver logado, retorna false.
+   */
+  public static function getUserData()
+  {
+    return isset($_SESSION['usuario']) ? $_SESSION['usuario'] : false;
+  }
+
+  /**
+   * Redireciona o usuário para uma página específica após o login ou logout.
+   *
+   * @param string $page A URL da página para redirecionar o usuário.
+   * @return void
+   */
+  public static function redirectTo($page)
+  {
+    header('Location: ' . $page);
+    exit;
+  }
+
+  /**
+   * Redireciona o usuário para a página de login.
+   *
+   * @return void
+   */
+  private static function redirectToLogin()
+  {
+    self::redirectTo($_ENV['VIEWS'] . '/auth/login.php');
+  }
+}
