3a34d2fe2c68f3c6b92c4a8669446e8d6b4a5369
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-24 01:51:47 -0300

    changes

--- grease/resources/views/admin/material/entrada.index.php
+++ grease/resources/views/admin/material/entrada.index.php
@@ -1,20 +1,28 @@
 <?php
 # ------ Configurações Básicas
-require dirname(dirname(dirname(dirname(__DIR__)))) . '\config.php';
+require dirname(dirname(dirname(dirname(__DIR__)))) . '/config.php';
 global $_ENV;
 
-import_utils(['auth', 'extend_styles', 'render_component']);
+import_utils(['auth']);
 
 Auth::check('adm');
+ 
+import_utils([
+  'extend_styles', 
+  'use_js_scripts', 
+  'render_component',
+  'Money'
+]);
 
 include $_ENV['PASTA_CONTROLLER'] . '/EntradaMaterial/ConsultaController.php';
-//print_r($saidas);
+//print_r($entradas);
 ?>
 
 
 <!------- HEAD --------->
 <?php
-require $_ENV['PASTA_VIEWS'] . '/components/head.php';
+render_component('head');
+extend_styles([ 'css.admin.financas' ]);
 ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
   integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
@@ -23,91 +31,97 @@ require $_ENV['PASTA_VIEWS'] . '/components/head.php';
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
 
 <title>
-  Admin 🕺 Grease
+  Finanças Admin 🕺 Grease
 </title>
 <!-------/ HEAD --------->
 
 
-<!-------/ BODY --------->
+<!------- BODY --------->
 <body>
   <?php
-  require $_ENV['PASTA_VIEWS'] . '/components/header.php';
+  render_component('sidebar');
   ?>
- <br><br>
 
-  <table id="myTable" class="display">
-    <thead>
-      <tr>
-        <th>Usurio</th>
-        <th>Material</th>
-        <th>Foto</th>
-        <th>Quantidade Entrada</th>
-        <th>Valor Pago</th>
-        <th>Estoque Atual</th>
-      </tr>
-    </thead>
+  <section class="dashboard">
+      <div class="top"> <i class="uil uil-bars sidebar-toggle"></i> </div>
+      <div class="dash-content" style="width: 80vw;">
+        <div style="text-align: right;">
+          <a href="<?php echo $_ENV['ROUTE'] ?>admin.material.create" class="button-link">
+            Novo Material
+          </a>
+          <span class="button-separator">|</span>
+          <a href="<?php echo $_ENV['ROUTE'] ?>admin.material.entrada.index" class="button-link">
+            Entradas
+          </a>
+          <span class="button-separator">|</span>
+          <a href="<?php echo $_ENV['ROUTE'] ?>admin.material.saida.index" class="button-link">
+            Saidas
+          </a>
+        </div>
 
-    <tbody>
-      <?php if ($entradas): ?>
-        <?php foreach ($entradas as $entrada): ?>
-        <tr>
-          <td>
-            <a href="<?= $_ENV['VIEWS']; ?>/admin/usuario/show.php?id=<?= $entrada['usuario_id']; ?>">
-              <?= $entrada['nome_usuario']; ?>
-            </a>
-          </td>
-          <td>
-            <a href="<?= $_ENV['VIEWS']; ?>/admin/material/show.php?id=<?= $entrada['material_id']; ?>">
-              <?= $entrada['nome']; ?>
-            </a>
-          </td>
-          <td>
-            <img 
-              width="100px" 
-              src="<?= $_ENV['STORAGE'] . '/image/material/' . $entrada['foto_material']; ?>"
-              alt="<?= $entrada['nome']; ?>" 
-            />
-          </td>
-          <td>
-            <?= $entrada['qtde_compra']; ?>
-          </td>
-          <td>
-            <?= $entrada['valor_gasto']; ?>
-          </td>
-          <td>
-            <?= $entrada['estoque_atual']; ?>
-          </td>
-          <!-- 
-          <td>
-            <a href="<?= $_ENV['VIEWS']; ?>/admin/material/show.php?id=<?php echo $material['material_id']; ?>">
-              <i class="fa-regular fa-eye"></i>
-            </a>
+        <div class="title"> <span class="text">Entradas | Materiais</span> </div>
+        <?php if (isset($entradas) && !empty($entradas)) { ?>
+        <table id="myTable" class="display">
+          <thead>
+            <tr>
+              <th>Usurio</th>
+              <th>Material</th>
+              <th>Foto</th>
+              <th>Quantidade Entrada</th>
+              <th>Valor Pago</th>
+              <th>Estoque Atual</th>
+            </tr>
+          </thead>
 
-            <br><br>
-            <a href="<?= $_ENV['URL_CONTROLLERS']; ?>/Material/EditController.php?id=<?= $material['material_id']; ?>">
-              <i class="fa-regular fa-pen-to-square"></i>
-            </a>
-            <br><br>
-
-            <a href="<?= $_ENV['URL_CONTROLLERS']; ?>/Material/DeletarController.php?id=<?= $material['material_id']; ?>">
-              <i class="fa-solid fa-trash"></i>
-            </a>
-          </td> -->
-        </tr>
-        <?php endforeach; ?>
-      <?php endif; ?>
-    </tbody>
-  </table>
-
+          <tbody>
+            
+              <?php foreach ($entradas as $entrada): ?>
+              <tr>
+                <td>
+                  <a href="<?= $_ENV['VIEWS']; ?>/admin/usuario/show.php?id=<?= $entrada['usuario_id']; ?>">
+                    <?= $entrada['nome_usuario']; ?>
+                  </a>
+                </td>
+                <td>
+                  <a href="<?= $_ENV['VIEWS']; ?>/admin/material/show.php?id=<?= $entrada['material_id']; ?>">
+                    <?= $entrada['nome_material']; ?>
+                  </a>
+                </td>
+                <td>
+                  <img 
+                    width="100px" 
+                    src="<?= $_ENV['STORAGE'] . '/image/material/' . $entrada['foto_material']; ?>"
+                    alt="<?= $entrada['nome_material']; ?>" 
+                  />
+                </td>
+                <td>
+                  <?= $entrada['qtde_compra']; ?>
+                </td>
+                <td>
+                  <?= Money::format($entrada['valor_gasto']); ?>
+                </td>
+                <td>
+                  <?= $entrada['estoque_atual']; ?>
+                </td>
+              </tr>
+              <?php endforeach; ?>
+              </tbody>
+        </table>
+        <?php } else { ?>
+          <h3>Sem inserções</h3>
+        <?php } ?>
+      </div>
+    </div>
+  </section>
 
   <?php
-  require $_ENV['PASTA_VIEWS'] . '/components/footer.php';
+  use_js_scripts([ 'js.admin.financas' ]);
   ?>
-
   <script type="text/javascript">
     $(document).ready(function () {
       $('#myTable').DataTable();
     });
+
   </script>
 </body>
 <!-------/ BODY --------->
