2def7aabb25cc5315c4dec7d74eff82f40cf77d3
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-24 01:50:17 -0300

    changes

--- grease/resources/views/admin/make-of/create.php
+++ grease/resources/views/admin/make-of/create.php
@@ -1,11 +1,18 @@
 <?php
 # ------ Configurações Básicas
-require dirname(dirname(dirname(dirname(__DIR__)))) . '\config.php';
+require dirname(dirname(dirname(dirname(__DIR__)))) . '/config.php';
 global $_ENV;
 
-import_utils(['auth', 'extend_styles', 'render_component']);
+import_utils(['auth']);
 
 Auth::check('adm');
+ 
+import_utils([
+  'extend_styles', 
+  'use_js_scripts', 
+  'render_component',
+  'Money'
+]);
 
 // Verifica se a variável de sessão 'ultimo_acesso' já existe
 if(isset($_SESSION['ultimo_acesso'])) {
@@ -22,19 +29,17 @@ if(isset($_SESSION['ultimo_acesso'])) {
 <!------- HEAD --------->
 <?php
 render_component('head');
-//extend_styles(['styles']);
+extend_styles([ 'css.admin.financas' ]);
 ?>
-
 <title>
-    Admin 🕺 Grease
+  Admin 🕺 Grease
 </title>
-<script src="https://cdn.jsdelivr.net/gh/plentz/jquery-maskmoney@master/dist/jquery.maskMoney.min.js"
-  type="text/javascript"></script>
 <!------- /HEAD --------->
+
 
 <body>
   <?php
-  require $_ENV['PASTA_VIEWS'] . '/components/header.php';
+  render_component('sidebar');
   ?>
 
   <?php if (isset($_SESSION['fed_makeof']) && !empty($_SESSION['fed_makeof'])): ?>
@@ -49,51 +54,65 @@ render_component('head');
   </script>
   <?php endif; ?>
 
-  <form 
-    action="<?= $_ENV["URL_CONTROLLERS"]; ?>/MakeOf/CreateController.php" 
-    method="POST"
-    id="frm-entrada"
-  >
-    <input 
-      type="hidden" 
-      name="usuario_id" 
-      value="<?= $_SESSION['usuario']['usuario_id'] ?>" 
-    />
-    <br>    
 
-    <label for="titulo">Titulo:</label><br>
-    <input 
-      type="text" 
-      name="titulo" 
-      required
-    />
+   <section class="dashboard">
 
-    <br>
-    <label for="descricao">Descrição:</label><br>
-    <textarea 
-      name="descricao" 
-      id="" 
-      cols="30" 
-      rows="10" 
-      required 
-    >
-    </textarea>
-    <br>
+    <div class="top">
+      <i class="uil uil-bars sidebar-toggle"></i>
+    </div>
+    <div class="dash-content">
+        <div class="overview">
+          <div class="title">
+            <span class="text">Cadastro Make Of</span>
+          </div>
 
-    <label for="uri">Link Make Ofs</label>
-    <input 
-      type="text" 
-      name="uri" 
-      required      
-    />   
-    <br>
-    
-    <input type="submit" value="salvar">
-  </form>
+          <form 
+            action="<?= $_ENV["URL_CONTROLLERS"]; ?>/MakeOf/CreateController.php" 
+            method="POST"
+            id="frm-entrada"
+          >
+          <input 
+            type="hidden" 
+            name="usuario_id" 
+            value="<?= $_SESSION['usuario']['usuario_id'] ?>" 
+          />
+
+          <label for="titulo">Titulo:</label><br>
+          <input 
+            type="text" 
+            name="titulo" 
+            required
+          />
+          <br><br>
+
+          <br>
+          <label for="descricao">Descrição:</label><br>
+          <textarea 
+            name="descricao" 
+            id="" 
+            cols="30" 
+            rows="10" 
+            required 
+          >
+          </textarea>
+          <br><br>
+
+          <label for="uri">Link Make Ofs</label>
+          <input 
+            type="text" 
+            name="uri" 
+            required      
+          />   
+          <br><br>
+          
+          <input type="submit" value="salvar">
+        </form>
+      </div>
+    </div>
+  </section>
+
 
   <?php
-  render_component('footer');
-  ?>
-  <script>
-  </script>
+  use_js_scripts([ 'js.admin.financas' ]);
+  ?> 
 </body>
