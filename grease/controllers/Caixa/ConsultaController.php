cd503f0d36d0a0aae758291958ed98b086e1e695
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-26 21:48:02 -0300

    changes

--- grease/controllers/Caixa/ConsultaController.php
+++ grease/controllers/Caixa/ConsultaController.php
@@ -7,10 +7,36 @@ import_utils([ 'valida_campo', 'navegate' ]);
 
 # ----- Consulta Caixa
 $caixa = new Caixa($mysqli);
- 
+$meta = new Meta($mysqli);
+
+$dadosDespesasReceitasPorMes = $caixa->obterDadosDespesasReceitasPorMes();
+
+$totalReceitas = array_sum($dadosDespesasReceitasPorMes['receitas']);
+$totalDespesas = array_sum($dadosDespesasReceitasPorMes['despesas']);
+
+$porcentagemDespesas = ($totalDespesas / ($totalReceitas + $totalDespesas)) * 100;
+$porcentagemReceitas = 100 - $porcentagemDespesas;
+
+
 return $data = [
-  'caixa'          => $caixa->buscarTodos(),
-  'saldo_atual'    => $caixa->obterSaldoAtual(),
-  'saldo_anterior' => $caixa->obterSaldoAnterior()
+  'caixa'             => $caixa->buscarTodos(),
+  'saldo_atual'       => $caixa->obterSaldoAtual(),
+  'saldo_anterior'    => $caixa->obterSaldoAnterior(),
+  'total_gasto'       => $caixa->obterTotalGasto(),
+  'total_necessario'  => $meta->obterTotalNecessarioAtivo(),
+
+  'meses'             => $dadosDespesasReceitasPorMes['meses'],
+  'receitas'          => $dadosDespesasReceitasPorMes['receitas'],
+  'despesas'          => $dadosDespesasReceitasPorMes['despesas'],
+  'saldos'            => $dadosDespesasReceitasPorMes['saldos'],
+
+  'dadosCategorias'   => $caixa->obterDadosCategorias(),
+  
+  'totalReceitas'        => $totalReceitas,
+  'totalDespesas'        => $totalDespesas,
+  
+  'porcentagemReceitas'  => $porcentagemReceitas,
+  'porcentagemDespesas'  => $porcentagemDespesas
 ];
+
 ?>
