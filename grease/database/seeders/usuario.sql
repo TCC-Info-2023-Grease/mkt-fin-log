3755cfc1a4611bbe7a5b6dacfa64e1a762c6d6ae
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-26 21:47:48 -0300

    changes

--- grease/database/seeders/usuario.sql
+++ grease/database/seeders/usuario.sql
@@ -1,18 +1,1 @@
--- USUARIOS
-INSERT INTO 
-  `usuarios` 
-  (
-    `usuario_id`, `tipo_usuario`, 
-    `nome`, `email`, 
-    `cpf`, `senha`, 
-    `idade`, `genero`, 
-    `celular`, `foto_perfil`
-  ) 
-VALUES
-  (
-    1, 'adm', 
-    'Master Potato', 'master.potato@gmail.com', 
-    '31276826893', MD5('Pa$$w0rd!'), 
-    10000, 'm', 
-    '(11) 98597-6152', NULL
-  );
+
