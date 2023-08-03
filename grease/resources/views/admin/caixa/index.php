0aa240aa10c8ba8c7f406e6216f6b6be9bc0a7fa
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-08-03 14:39:12 -0300

    Update index.php

--- grease/resources/views/admin/caixa/index.php
+++ grease/resources/views/admin/caixa/index.php
@@ -3,7 +3,7 @@
 require dirname(dirname(dirname(dirname(__DIR__)))) . '/config.php';
 global $_ENV;
 
-import_utils(['auth']);
+import_utils(['Auth']);
 
 Auth::check('adm');
  
@@ -252,137 +252,135 @@ extend_styles([ 'css.admin.financas' ]);
   use_js_scripts([ 'js.admin.financas' ]);
   ?>
   <script>
-    document.addEventListener("DOMContentLoaded", function () {
-      const ctx = document.getElementById("financasChart").getContext("2d");
+    class ChartCaixa {
+      static saldoMensal () {
+        const ctx = document.getElementById("financasChart").getContext("2d");
 
-      const chartData = {
-        labels: <?= json_encode($meses); ?>,
-        datasets: [
-          {
-            label: "Saldo",
-            data: <?= json_encode($saldos); ?>,
-            borderColor: "rgba(75, 192, 192, 1)",
-            backgroundColor: "rgba(75, 192, 192, 0.2)",
-            fill: true,
-          },
-        ],
-      };
+        const chartData = {
+          labels: <?= json_encode($meses); ?>,
+          datasets: [
+            {
+              label: "Saldo",
+              data: <?= json_encode($saldos); ?>,
+              borderColor: "rgba(75, 192, 192, 1)",
+              backgroundColor: "rgba(75, 192, 192, 0.2)",
+              fill: true,
+            },
+          ],
+        };
 
-      const chartConfig = {
-        type: "line",
-        data: chartData,
-        options: {
-          responsive: true,
-          scales: {
-            x: {
-              display: true,
-              title: {
+        const chartConfig = {
+          type: "line",
+          data: chartData,
+          options: {
+            responsive: true,
+            scales: {
+              x: {
                 display: true,
-                text: "Mês",
+                title: {
+                  display: true,
+                  text: "Mês",
+                },
+              },
+              y: {
+                display: true,
+                title: {
+                  display: true,
+                  text: "Saldo",
+                },
               },
             },
-            y: {
-              display: true,
-              title: {
-                display: true,
-                text: "Saldo",
-              },
-            },
           },
-        },
-      };
+        };
 
-      new Chart(ctx, chartConfig);
-    });
-  </script>
-
-
-  <script>
-    document.addEventListener("DOMContentLoaded", function () {
-      const ctx2 = document.getElementById("despesasReceitasChart").getContext("2d");
-
-      const chartData2 = {
-        labels: ["Receitas", "Despesas"],
-        datasets: [
-          {
-            data: [<?= $porcentagemReceitas; ?>, <?= $porcentagemDespesas; ?>],
-            backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
-            borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
-            borderWidth: 1,
-          },
-        ],
-      };
-
-      const chartConfig2 = {
-        type: "pie",
-        data: chartData2,
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              display: true,
-              position: "bottom",
-            },
-          },
-        },
-      };
-
-      new Chart(ctx2, chartConfig2);
-    });
-  </script>
-
-
- <script>
-  // Função para criar o gráfico de barras
-  function createBarChart() {
-    const dadosCategorias = <?php echo json_encode($dadosCategorias); ?>;
-
-    let labelsCategorias = dadosCategorias.map(item => item.categoria);
-
-    let totalDespesas = dadosCategorias.map(item => item.total_despesa);
-    let totalReceitas = dadosCategorias.map(item => item.total_receita);
-
-    
-    let ctxCategorias = document.getElementById('categoriasChart').getContext('2d');
-    let categoriasChart = new Chart(ctxCategorias, {
-      type: 'bar',
-      data: {
-        labels: labelsCategorias,
-        datasets: [
-          {
-            label: 'Despesas',
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-            data: totalDespesas,
-          },
-          {
-            label: 'Receitas',
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1,
-            data: totalReceitas,
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            stacked: true,
-          },
-          y: {
-            beginAtZero: true,
-          }
-        }
+        new Chart(ctx, chartConfig);
       }
+
+      static despesasReceitas() {
+        const ctx2 = document.getElementById("despesasReceitasChart").getContext("2d");
+
+        const chartData2 = {
+          labels: ["Receitas", "Despesas"],
+          datasets: [
+            {
+              data: [<?= $porcentagemReceitas; ?>, <?= $porcentagemDespesas; ?>],
+              backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
+              borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
+              borderWidth: 1,
+            },
+          ],
+        };
+
+        const chartConfig2 = {
+          type: "pie",
+          data: chartData2,
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+                display: true,
+                position: "bottom",
+              },
+            },
+          },
+        };
+
+        new Chart(ctx2, chartConfig2);
+      }
+
+      static receitasDespesasPorCategoria() {
+        const dadosCategorias = <?php echo json_encode($dadosCategorias); ?>;
+
+        let labelsCategorias = dadosCategorias.map(item => item.categoria);
+
+        let totalDespesas = dadosCategorias.map(item => item.total_despesa);
+        let totalReceitas = dadosCategorias.map(item => item.total_receita);
+
+        
+        let ctxCategorias = document.getElementById('categoriasChart').getContext('2d');
+        let categoriasChart = new Chart(ctxCategorias, {
+          type: 'bar',
+          data: {
+            labels: labelsCategorias,
+            datasets: [
+              {
+                label: 'Despesas',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+                data: totalDespesas,
+              },
+              {
+                label: 'Receitas',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                data: totalReceitas,
+              }
+            ]
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+              x: {
+                stacked: true,
+              },
+              y: {
+                beginAtZero: true,
+              }
+            }
+          }
+        });
+      }
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+      ChartCaixa.saldoMensal();
+      ChartCaixa.despesasReceitas();
+      ChartCaixa.receitasDespesasPorCategoria();
     });
-  }
-
-  document.addEventListener('DOMContentLoaded', createBarChart);
-</script>
-
+  </script>
 
 
   <script type="text/javascript">
