2fd34c201e7372a6f64fe107f0d6b7ddc88838e0
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-08-17 17:43:17 -0300

    changes

--- grease/controllers/Sala/ConsultaController.php
+++ grease/controllers/Sala/ConsultaController.php
@@ -6,7 +6,16 @@ import_utils(['valida_campo', 'navegate']);
 # ----- Consulta Usuário
 $sala = new Sala($mysqli);
  
+$porcentagemDevedoresPagantes = $sala->porcentagemDevedoresPagantes();
+
 return $data = [
-  'sala' => $sala->obterReceitasComAluno()
+  'sala' => $sala->obterReceitasComAluno(),
+  'agruparPagamentosPorMes'      => $sala->agruparPagamentosPorMes(),
+  'totalAlunosPagantes'          => $sala->calcularTotalAlunosPagantes(),
+  'totalPagamentos'              => $sala->calcularTotalPagamentos(),
+  'alunosDevedores'              => $sala->obterAlunosDevedores(),
+  'rankingTopPagantes'           => $sala->calcularRankingTopPagantes(),
+  'porcentagem_devedores'        => $porcentagemDevedoresPagantes['porcentagem_devedores'],
+  'porcentagem_pagantes'         => $porcentagemDevedoresPagantes['porcentagem_pagantes']
 ];
 ?>
