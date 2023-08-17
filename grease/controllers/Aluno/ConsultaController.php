cd2f39bc87ee49c79bda3cd9686136598dccb8ba
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-08-17 20:37:43 -0300

    changes

---
+++ grease/controllers/Aluno/ConsultaController.php
@@ -0,0 +1,21 @@
+<?php  
+# ------ Dados Iniciais
+global $mysqli;
+import_utils(['valida_campo', 'navegate']);
+
+# ----- Consulta Usuário
+$sala = new Sala($mysqli);
+ 
+$porcentagemDevedoresPagantes = $sala->porcentagemDevedoresPagantes();
+
+return $data = [
+  'sala' => $sala->obterReceitasComAluno(),
+  'pagamentosPorMes'      => $sala->agruparPagamentosPorMes(),
+  'totalAlunosPagantes'          => $sala->calcularTotalAlunosPagantes(),
+  'totalPagamentos'              => $sala->calcularTotalPagamentos(),
+  'alunosDevedores'              => $sala->obterAlunosDevedores(),
+  'rankingTopPagantes'           => $sala->calcularRankingTopPagantes(),
+  'porcentagem_devedores'        => $porcentagemDevedoresPagantes['porcentagem_devedores'],
+  'porcentagem_pagantes'         => $porcentagemDevedoresPagantes['porcentagem_pagantes']
+];
+?>
