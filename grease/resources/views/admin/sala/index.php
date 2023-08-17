c44d46258957b573a0c7e0e8560fe5621a145528
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-08-17 20:38:59 -0300

    changes

--- grease/resources/views/admin/sala/index.php
+++ grease/resources/views/admin/sala/index.php
@@ -16,7 +16,7 @@ import_utils([
 
 include $_ENV['PASTA_CONTROLLER'] . '/Sala/ConsultaController.php';
 
-var_dump($data);
+//var_dump($data);
 
 if (isset($_SESSION['ultimo_acesso'])) {
     $ultimo_acesso = $_SESSION['ultimo_acesso'];
@@ -137,33 +137,147 @@ extend_styles(['css.admin.financas']);
              <div class="title"><span class="text">Pagamentos por mês</span></div>
 
             <details>
-              <summary>Ver mais...</summary>
-
-              <div class="chart-container" style="width: 100%;">
+                <summary>Ver mais...</summary>
                 <center>
-                  <canvas id="financasChart" style="max-width: 500px;"></canvas>
+
+                <style type="text/css">
+                    table.pagamentosPorMes {
+                        border-radius: 12px;
+                        margin: 2rem 0;
+                        /*border: 3px solid rgba(0, 0, 0, 0.7);
+                        padding: 1rem;*/
+                    }
+
+                    table.pagamentosPorMes thead {
+                        background: #f3f3f3;
+                        border: 3px solid black;
+                    }
+
+                    table.pagamentosPorMes th {
+                        padding: 0.5rem;
+                    }
+
+                    table.pagamentosPorMes td {
+                        padding: 1rem;
+                    }
+                </style>
+
+                <table class="pagamentosPorMes" style="width: 80%!important;text-align: center;" cellpadding="19px" cellspacing="5px"> 
+                    <thead>
+                        <tr>
+                            <th>Mês</th>
+                            <th>Valor Total</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <style type="text/css">
+                        th a i {
+                            font-size: 1.7rem;
+                        }
+                        </style>
+                        <?php foreach ($data['pagamentosPorMes'] as $item): ?>
+                            <tr>
+                                <td>
+                                    <?= $item['mes'] ?>
+                                </td>
+                                <td>
+                                    <?= Money::format($item['total']) ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
                 </center>
-              </div>
             </details>
           </div>
         </div>
 
          <div class="dash-content">
           <div class="dash-estatistics">
-                <div class="title"><span class="text">Top Pagantes</span></div>
+                <div class="title"><span class="text">Top Pagadores</span></div>
 
                 <details>
                   <summary>Ver mais...</summary>
-                  <div class="chart-container">
-                    <style type="text/css">
-                      #despesasReceitasChart {
-                        height: 300px!important;
+                  
+                  <br>        
+
+                  <style>
+                      .rank div {
+                        width: 150px;
+                        padding: 10px;
+                        display: inline-block;
+                        word-break: break-all;
+                        height: 50px;
+                        background: whitesmoke;
+                        color: whitesmoke;
+                        border-radius: 13px;
+                        border-bottom: 2px solid #3a3b3c;
                       }
-                    </style>
-                    <center>
-                    <canvas id="despesasReceitasChart" style="max-width: 300px;"></canvas>
+
+                      .rank div:hover {
+                        cursor: pointer;
+                        transform: scale(1.3);
+                        transition: .45s all;
+                      }
+
+                      .rank div:not(:last-child) {
+                        margin-right: 35px;
+                      }
+
+                      .rank .rank__1 {
+                        height: 120px;
+                        background: #fe9b35;
+                      }
+                      .rank .rank__2 {
+                        height: 90px;
+                        background: #9e9c9c;
+                      }
+                      .rank .rank__3 {
+                        height: 60px;
+                        background: #9a4b0ab5;
+                      }
+
+                      .rank div {
+                        display: flex;
+                        justify-content: center;
+                        align-content: center;
+                      } 
+                  </style>
+                    <center 
+                        style="    
+                            display: flex;
+                            justify-content: center;
+                            align-items: flex-end;
+                            padding: 23px;
+                            width: 100%;
+                        " 
+                        class="rank"
+                    >
+                        <div class="rank__3">
+                        <?= 
+                            isset($data['rankingTopPagantes'][2]['nome_aluno']) ||
+                            !empty($data['rankingTopPagantes'][2]['nome_aluno'])? 
+                                $data['rankingTopPagantes'][2]['nome_aluno'] : 'N/A'; 
+                        ?> 
+                        </div>
+
+                        <div class="rank__1">
+                        <?= 
+                            isset($data['rankingTopPagantes'][0]['nome_aluno']) ||
+                            !empty($data['rankingTopPagantes'][0]['nome_aluno'])? 
+                                $data['rankingTopPagantes'][0]['nome_aluno'] : 'N/A'; 
+                        ?> 
+                        </div>
+
+                        <div class="rank__2">
+                        <?= 
+                            isset($data['rankingTopPagantes'][1]['nome_aluno']) ||
+                            !empty($data['rankingTopPagantes'][1]['nome_aluno'])? 
+                                $data['rankingTopPagantes'][1]['nome_aluno'] : 'N/A'; 
+                        ?>      
+                        </div>
                     </center>
-                  </div>
                 </details>
           </div>
         </div>
@@ -244,11 +358,6 @@ extend_styles(['css.admin.financas']);
                     </thead>
 
                     <tbody>
-                        <style type="text/css">
-                        th a i {
-                            font-size: 1.7rem;
-                        }
-                        </style>
                         <?php foreach ($data['sala'] as $sala): ?>
                         <tr>
                             <td>
@@ -263,7 +372,7 @@ extend_styles(['css.admin.financas']);
                             <th style="padding: 26px;">
                                 <a href="<?= $_ENV['URL_CONTROLLERS']; ?>/Sala/ShowController.php?id=<?= $sala['caixa_id']; ?>"
                                     class="icon-link ">
-                                    <i class="fa-regular fa-eye"></i>
+                                    <i class="fa-regular fa-eye" style="font-size: 1.15rem!important;"></i>
                                 </a>
                                 <br>
                                 <br>
@@ -278,7 +387,7 @@ extend_styles(['css.admin.financas']);
                                 <a href="#" onclick="if (confirm('Deseja editar mesmo?')) {
                                        this.href = '<?= $_ENV['URL_CONTROLLERS']; ?>/Sala/EditController.php?id=<?= $sala['caixa_id']; ?>';
                                    }" class="icon-link edit">
-                                    <i class="fa-solid fa-pen"></i>
+                                    <i class="fa-solid fa-pen" style="font-size: 1.15rem!important;"></i>
                                 </a>
                             </th>
                         </tr>
@@ -304,9 +413,9 @@ extend_styles(['css.admin.financas']);
         ]);
     ?>    
     <script type="text/javascript">
-    $(document).ready(function() {
-        $('#myTable').DataTable();
-    });
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
     </script>
 </body>
 <!-------/ BODY --------->
