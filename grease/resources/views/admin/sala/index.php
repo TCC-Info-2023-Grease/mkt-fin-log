b3ecf864589a6a478f408c9379e8549e429864e5
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-08-17 17:44:10 -0300

    changes

--- grease/resources/views/admin/sala/index.php
+++ grease/resources/views/admin/sala/index.php
@@ -16,7 +16,7 @@ import_utils([
 
 include $_ENV['PASTA_CONTROLLER'] . '/Sala/ConsultaController.php';
 
-//var_dump($data);
+var_dump($data);
 
 if (isset($_SESSION['ultimo_acesso'])) {
     $ultimo_acesso = $_SESSION['ultimo_acesso'];
@@ -77,82 +77,232 @@ extend_styles(['css.admin.financas']);
 
 
         <div class="dash-content">
-            <div class="title"> <span class="text">Movimentações</span> </div>
+          <div class="boxes">
+             <div 
+                class="box 
+                <?php if ($data['totalPagamentos'] <= 0): ?>
+                    box3
+                <?php elseif ($data['totalPagamentos'] > 0): ?>
+                    box1
+                <?php endif; ?>
+              ">     
+                <span class="text">Saldo Atual</span> 
+                <span class="number">
+                   <?= Money::format($data['totalPagamentos']); ?>
+                </span> 
+              </div>
 
-            <?php if (isset($data['sala']) && !empty($data['sala'])) { ?>
-            <table id="myTable" class="display">
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Valor</th>
-                        <th>Data</th>
-                        <th>Descrição</th>
-                        <th>Obs</th>
-                        <th>Tipo Movimentação</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
+              <div 
+                class="box 
+                <?php if ($data['alunosDevedores']['totalDevedores'] == 0): ?>
+                    box1
+                <?php elseif (
+                    $data['alunosDevedores']['totalDevedores'] >= 1 && 
+                    $data['alunosDevedores']['totalDevedores'] <= 10
+                ): ?>
+                    box3
+                <?php elseif ($data['alunosDevedores']['totalDevedores'] > 10): ?>
+                    box2
+                <?php endif; ?>
+              ">              
+                <span class="text">Total Devedores do Mês</span> 
+                <span class="number">
+                  <?= $data['alunosDevedores']['totalDevedores']; ?>
+                </span> 
+              </div>
 
-                <tbody>
+              <div class="box
+                <?php if ($data['totalAlunosPagantes'] <= 0): ?>
+                    box3
+                <?php elseif ($data['totalAlunosPagantes'] > 0): ?>
+                    box1
+                <?php endif; ?>
+              "> 
+                <span class="text">Total Alunos Pagantes</span> 
+                <span class="number">
+                  <?= $data['totalAlunosPagantes']; ?>
+                </span> 
+              </div>          
+            </div>
+        </div>
+
+        <div class="dash-content">
+            <hr color="black" />
+        </div>
+
+        <div class="dash-content">
+          <center style="margin-bottom: 46px;"><h2>Estatisticas</h2></center>
+
+          <div class="dash-estatistics">
+             <div class="title"><span class="text">Pagamentos por mês</span></div>
+
+            <details>
+              <summary>Ver mais...</summary>
+
+              <div class="chart-container" style="width: 100%;">
+                <center>
+                  <canvas id="financasChart" style="max-width: 500px;"></canvas>
+                </center>
+              </div>
+            </details>
+          </div>
+        </div>
+
+         <div class="dash-content">
+          <div class="dash-estatistics">
+                <div class="title"><span class="text">Top Pagantes</span></div>
+
+                <details>
+                  <summary>Ver mais...</summary>
+                  <div class="chart-container">
                     <style type="text/css">
-                    th a i {
-                        font-size: 1.7rem;
-                    }
+                      #despesasReceitasChart {
+                        height: 300px!important;
+                      }
                     </style>
-                    <?php foreach ($data['sala'] as $sala): ?>
-                    <tr>
-                        <td>
-                            <?= $sala['nome_usuario']; ?>
-                        </td>
-                        <td>
-                            <?= Money::format($sala['valor']); ?>
-                        </td>
-                        <td>
-                            <?= date('d/m/Y', strtotime($sala['data_movimentacao'])); ?>
-                        </td>
-                        <td>
-                            <?= $sala['tipo_movimentacao']; ?>
-                        </td>
-                        <td style="width: 100p;">
-                            <?= $sala['descricao']; ?>
-                        </td>
-                        <td style="width: 100p;">
-                            <?= $sala['obs']; ?>
-                        </td>
-                        <th style="padding: 26px;">
-                            <a href="<?= $_ENV['URL_CONTROLLERS']; ?>/Sala/ShowController.php?id=<?= $sala['caixa_id']; ?>"
-                                class="icon-link ">
-                                <i class="fa-regular fa-eye"></i>
-                            </a>
-                            <br>
-                            <br>
-                            <hr>
-                            <br>
+                    <center>
+                    <canvas id="despesasReceitasChart" style="max-width: 300px;"></canvas>
+                    </center>
+                  </div>
+                </details>
+          </div>
+        </div>
 
-                            <!-- <a href="#" onclick="if (confirm('Deseja excluir mesmo?')) {
-                                   this.href = '<?= $_ENV['URL_CONTROLLERS']; ?>/Sala/DeletarController.php?id=<?= $sala['caixa_id']; ?>';
-                               }" class="icon-link delete">
-                                <i class="fa-solid fa-trash"></i>
-                            </a> -->
-                            <a href="#" onclick="if (confirm('Deseja editar mesmo?')) {
-                                   this.href = '<?= $_ENV['URL_CONTROLLERS']; ?>/Sala/EditController.php?id=<?= $sala['caixa_id']; ?>';
-                               }" class="icon-link edit">
-                                <i class="fa-solid fa-pen"></i>
-                            </a>
-                        </th>
-                    </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+        <div class="dash-content">
+          <div class="dash-estatistics">
+            <div class="title"><span class="text">Porcentagem Devedores X Pagadores</span></div>
+
+            <details>
+              <summary>Ver mais...</summary>
+                <br>        
+
+                <center style="    
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-around;
+                    flex-direction: row;
+                    flex-wrap: wrap;
+                    padding: 23px;
+                ">
+                    <span style="
+                    ">
+                        <strong style="
+                            border: 2px solid black; padding: 12px;border-radius: 4px;margin-right: 3px;
+                            background: #55f954;
+                        ">
+                            Pagadores:
+                        </strong> 
+                        <span style="border: 2px solid black; padding: 12px;border-radius: 4px;margin-right: 12px;">
+                            <?= abs(round($data['porcentagem_pagantes'], 1)); ?>%
+                        </span>
+                    </span>
+                    <span>
+                        <strong style="
+                            border: 2px solid black; padding: 12px;border-radius: 4px;margin-right: 3px;
+                            background: #ff0b65;
+                        ">
+                            Devedores:
+                        </strong> 
+                        <span style="border: 2px solid black; padding: 12px;border-radius: 4px;margin-right: 12px;">
+                            <?= abs(round($data['porcentagem_devedores'], 1)); ?>%
+                        </span>
+                    </span>
+                </center>
+                <br>
+            </details>
+          </div>
+
+          <br>
+        </div>
+
+        <div class="dash-content">
+            <hr color="black" />
+        </div>
+
+        <div class="dash-content" style="padding-top: 0px;">
+            <?php if (isset($data['sala']) && !empty($data['sala'])) { ?>
+                <div style="display: flex;justify-content: space-between;align-items: center;">
+                    <div class="title"><span class="text">Movimentações</span></div>
+
+                    <div class="dropdown">
+                      <button onclick="toggleDropdown()" class="dropbtn">Exportar</button>
+                      <div id="myDropdown" class="dropdown-content">
+                        <button onclick="exportToPDF()">PDF</button>
+                        <button onclick="exportToExcel()">Excel</button>
+                      </div>
+                    </div>
+                </div>
+
+                <table id="myTable" class="display" style="width: 100%!important;"> 
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Valor</th>
+                            <th>Data</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <style type="text/css">
+                        th a i {
+                            font-size: 1.7rem;
+                        }
+                        </style>
+                        <?php foreach ($data['sala'] as $sala): ?>
+                        <tr>
+                            <td>
+                                <?= $sala['nome_aluno']; ?>
+                            </td>
+                            <td>
+                                <?= Money::format($sala['valor']); ?>
+                            </td>
+                            <td>
+                                <?= date('d/m/Y', strtotime($sala['data_movimentacao'])); ?>
+                            </td>
+                            <th style="padding: 26px;">
+                                <a href="<?= $_ENV['URL_CONTROLLERS']; ?>/Sala/ShowController.php?id=<?= $sala['caixa_id']; ?>"
+                                    class="icon-link ">
+                                    <i class="fa-regular fa-eye"></i>
+                                </a>
+                                <br>
+                                <br>
+                                <hr>
+                                <br>
+
+                                <!-- <a href="#" onclick="if (confirm('Deseja excluir mesmo?')) {
+                                       this.href = '<?php //$_ENV['URL_CONTROLLERS']; ?>/Sala/DeletarController.php?id=<?php //$sala['caixa_id']; ?>';
+                                   }" class="icon-link delete">
+                                    <i class="fa-solid fa-trash"></i>
+                                </a> -->
+                                <a href="#" onclick="if (confirm('Deseja editar mesmo?')) {
+                                       this.href = '<?= $_ENV['URL_CONTROLLERS']; ?>/Sala/EditController.php?id=<?= $sala['caixa_id']; ?>';
+                                   }" class="icon-link edit">
+                                    <i class="fa-solid fa-pen"></i>
+                                </a>
+                            </th>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
             <?php } else { ?>
             <h3>Sem dados</h3>
             <?php } ?>
         </div>
+
+        <br><br>
     </section>
 
     <?php
-    use_js_scripts(['js.admin.financas']);
-    ?>
+        use_js_scripts([ 
+          'js.lib.xlsx',
+          'js.lib.jspdf',
+          'js.services.ChartCaixa',
+          'js.services.jspdf_plugin_autotable',
+          'js.services.ExportTabelaCaixa',
+          'js.admin.financas'
+        ]);
+    ?>    
     <script type="text/javascript">
     $(document).ready(function() {
         $('#myTable').DataTable();
