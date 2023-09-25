ffe4a6d953db97066deb0b40958edea78ddd3e16
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-09-25 16:24:30 -0300

    Update CadastroController.php

--- grease/controllers/Conta/CadastroController.php
+++ grease/controllers/Conta/CadastroController.php
@@ -19,7 +19,12 @@ $_SESSION['ultimo_acesso'] = time();
 
 # ------ Validar Envio de Dados
 $campos_validos = (
-  !empty($_POST['nome']) && isset($_POST['nome'])
+  !empty($_POST['titulo']) && isset($_POST['titulo']) &&
+  !empty($_POST['descricao']) && isset($_POST['descricao']) &&
+  !empty($_POST['valor']) && isset($_POST['valor'])  &&
+  !empty($_POST['data_validade']) && isset($_POST['data_validade']) &&
+  !empty($_POST['usuario_id']) && isset($_POST['usuario_id']) &&
+  !empty($_POST['fornecedor_id']) && isset($_POST['fornecedor_id']) 
 );
 if (!$campos_validos) 
 {
